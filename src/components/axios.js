11642178b0aec1b1d8434cd02fed443f56bd3a16
ShravanKumarD <97720033+ShravanKumarD@users.noreply.github.com>
2024-10-04 14:50:50 +0530

    fix

--- src/components/axios.js
+++ src/components/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://13.127.116.191:3000';
+axios.defaults.baseURL = 'http://13.127.116.191:3000';
 // axios.defaults.baseURL = 'http://localhost:3000';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 
